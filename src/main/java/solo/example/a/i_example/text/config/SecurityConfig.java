fed063dfd1bb4eaba34fb75d858d71bcfc946ca1
jominhyeong97 <jumbo0303@daum.net>
2025-09-20 04:51:38 +0900

    config 설정중

---
+++ src/main/java/solo/example/a/i_example/text/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package solo.example.a.i_example.text.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+
+@Configuration
+@RequiredArgsConstructor
+
+public class SecurityConfig {
+
+    private CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.cookinghansu.shop"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
